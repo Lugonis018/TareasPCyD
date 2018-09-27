5915cca34b988e440bb3a220180f106b61851f6f
AUTONOMA <AUTONOMA@L02PC25>
2018-09-27 08:56:17 -0500

    ESto inicia+

---
+++ src/proyectochat/Cliente.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectochat;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+/**
+ *
+ * @author AUTONOMA
+ */
+public class Cliente extends javax.swing.JApplet implements ActionListener,Runnable {
+
+    @Override
+    public void init() {
+        try {
+            initComponents();
+            btnSend.addActionListener(this);
+            txtNick.requestFocus();
+            Thread hiloCliente=new Thread(this);
+            hiloCliente.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtAreaChatC = new javax.swing.JTextArea();
+        txtMessage = new javax.swing.JTextField();
+        btnSend = new javax.swing.JButton();
+        txtNick = new javax.swing.JTextField();
+        txtIp = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
+        txtAreaChatC.setColumns(20);
+        txtAreaChatC.setRows(5);
+        jScrollPane1.setViewportView(txtAreaChatC);
+
+        btnSend.setText("Enviar");
+        btnSend.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSendActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Nickname :");
+
+        jLabel2.setText("Destinatario :");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel3.setText("- CHAT-");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtMessage)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnSend))
+                    .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtNick, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(40, 40, 40)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9, Short.MAX_VALUE)
+                        .addComponent(txtIp, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel3)
+                .addGap(145, 145, 145))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtNick, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtIp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtMessage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnSend))
+                .addContainerGap())
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnSendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSendActionPerformed
+        txtMessage.setText("");
+        txtMessage.requestFocus();
+    }//GEN-LAST:event_btnSendActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnSend;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea txtAreaChatC;
+    private javax.swing.JTextField txtIp;
+    private javax.swing.JTextField txtMessage;
+    private javax.swing.JTextField txtNick;
+    // End of variables declaration//GEN-END:variables
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==btnSend)
+        {
+            
+            try
+            {
+                Socket skCliente =new Socket("localhost",8888);
+                Envio Datos=new Envio();
+                Datos.setNick(txtNick.getText());
+                Datos.setIp(txtIp.getText());
+                Datos.setMessage(txtMessage.getText());
+                
+                ObjectOutputStream paq_datos = new ObjectOutputStream(skCliente.getOutputStream());
+                paq_datos.writeObject(Datos);
+                txtAreaChatC.append("\n"+txtNick.getText()+": "+txtMessage.getText());
+                skCliente.close();
+                
+            }catch(IOException ex)
+            {
+                System.out.println("Ocurrió un Error "+ex.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        try{
+            ServerSocket serv_cli=new ServerSocket(8888);
+            Socket runCliente;
+            Envio paq_recibido;
+            while(true){
+                runCliente=serv_cli.accept();
+                ObjectInputStream dis=new ObjectInputStream(runCliente.getInputStream());
+                paq_recibido=(Envio)dis.readObject();
+                txtAreaChatC.append("\n"+paq_recibido.getNick()+": "+paq_recibido.getMessage());
+                runCliente.close();
+            }
+        }catch(Exception e){}
+    }
+}
---
+++ src/proyectochat/Envio.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectochat;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author AUTONOMA
+ */
+public class Envio implements Serializable{
+    private String nick,ip,message;
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+}
